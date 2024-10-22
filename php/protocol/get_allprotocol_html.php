163f3b46cf421f514efdecd897c4621744272ab5
NingLeiXueR <348634371@qq.com>
2024-10-22 11:57:15 +0800

    优化-规范化命名
    优化-规范化命名

---
+++ php/protocol/get_allprotocol_html.php
@@ -0,0 +1,8 @@
+<html>
+	<h2>查看协议</h2>
+	<form action="./get_allprotocol.php" method="post" accept-charset="UTF-8">
+		<?php require_once "../serverls.php";?>
+		<input type="submit" value="查看"/><br/>
+	</form>
+</body>
+</html>
