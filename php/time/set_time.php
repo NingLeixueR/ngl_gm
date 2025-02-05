654ecf42fabe14f67dd5981624b002511dc8a4fd
NingLeiXueR <348634371@qq.com>
2025-02-05 18:47:44 +0800

    优化-规范化命名
    优化-规范化命名

---
+++ php/time/set_time.php
@@ -0,0 +1,41 @@
+<?php
+ include '../SocketByte.php';
+ 
+ $so = new SocketByte();
+ if($so->connectServer($_POST['server']) == false)
+ {
+	 echo "connect err!!!";
+	 return;
+ }
+ 
+ $year = $_POST['year'];
+ $mon = $_POST['mon'];
+ $day = $_POST['day'];
+ $hour = $_POST['hour'];
+ $min = $_POST['min'];
+ $sec = $_POST['sec'];
+ $totime = mktime($hour, $min, $sec, $mon, $day, $year);
+ 
+ /*
+	需要发送给服务器的字段包括
+	actor_name  指定actor的类型  单例必须
+	actor_id	操作制定actor    非单例必须
+	operator	制定操作类型（自定义字符串）
+	data        可选数据	
+ */
+ 
+ 
+ $arr = array(
+	'actor_name' => 'ACTOR_NOTICE',
+	'operator' => 'set_time',
+	'data' => array(
+		'distributeall' => true,
+		'time' => $totime
+	)
+ );
+ 
+ $json = json_encode($arr); 
+ $so->send($json);
+ $response = $so->wait_response(); 
+
+?>
